31b7ba4e64974368ececa294266fb803e9cc143e
bitopan987 <bitopanxon1@gmail.com>
2022-04-27 18:01:34 +0530

    Refactor

--- src/components/CardComponent.js
+++ src/components/CardComponent.js
@@ -19,7 +19,7 @@ import DeleteIcon from '@mui/icons-material/Delete';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    width: '30%',
+    width: '33%',
     marginTop: '8rem',
   },
   content: {
